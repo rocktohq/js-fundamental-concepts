ed31eaf69a2ac6ba9996fe2c3580fdaf7ed1fa45
Saidul Mursalin <itzmonir@gmail.com>
2023-07-30 09:29:43 +0600

    Variables and Array

---
+++ array.js
@@ -0,0 +1,17 @@
+var names = ["Monir", "Rashed", "Imran", "Roman"];
+var ages = [30, 29, 31, 28];
+
+// Print Array
+// console.log(names);
+// console.log(names[0]);
+
+// Print Element by Index
+var name = names[2];
+// console.log(name);
+
+// Set a New Value by Index
+names[3] = "Ruman";
+// console.log(names);
+
+// Index of a Value
+// console.log(ages.indexOf(31));
---
+++ variable.js
@@ -0,0 +1,17 @@
+/*
+# Variable Declatarion [Has 5 Major Parts]
+# [1. Keyword] : var 
+# [2. Name] : myAge 
+# [3. AssignmentOperator] : = 
+# [4. Value] : 29
+# [5. EndPoint] : ;
+# Example: var myAge = 29;
+*/
+var eggPrice = 60;
+var myAge = 29;
+var temperature = 32;
+
+// console.log(eggPrice);
+
+var result = 3.33;
+// console.log(result.toFixed(1));
