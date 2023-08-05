1c1e4405d05f4033a1c7f68bc732bb83fc68332d
Saidul Mursalin <itzmonir@gmail.com>
2023-08-05 11:52:47 +0600

    Math.js

---
+++ module-21/math.js
@@ -0,0 +1,28 @@
+// Power
+const power = Math.pow(4, 2);
+// console.log(power);
+
+
+// Absolute Value
+const num1 = 25;
+const num2 = 45;
+const gap = Math.abs(num1 - num2);
+// console.log(gap);
+
+
+// Round Number
+const number = 3.544;
+const roundNumber = Math.round(number);
+// console.log(roundNumber);
+
+
+// Ceil and Floor
+const ceilNum = Math.ceil(4.0000000001);
+// console.log(ceilNum);
+const floorNum = Math.floor(4.999999999);
+// console.log(floorNum);
+
+
+// Random
+let randomNum = Math.round(Math.random() * 100);
+console.log(randomNum);
