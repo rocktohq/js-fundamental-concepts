533fd0a8b1fd4aa26b2c20b01cb836bcbe232da0
Saidul Mursalin <itzmonir@gmail.com>
2023-08-06 13:06:14 +0600

    Remove duplicate elements

---
+++ module-21/fibonacci2.js
@@ -0,0 +1,8 @@
+function calculateFibo(fiboNumber) {
+
+}
+
+const fiboNumber = 10;
+const fiboSeries = calculateFibo(fiboNumber);
+
+console.log(fiboSeries);
--- module-22/duplicate.js
+++ module-22/duplicate.js
@@ -5,7 +5,7 @@ function removeDuplicate(names) {
     const unique = [];
     for (let i = 0; i < names.length; i++) {
         const name = names[i];
-        if(unique.includes(name) === false){
+        if (unique.includes(name) === false) {
             unique.push(name);
         }
     }
