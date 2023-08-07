5867bddf4981fe238e8eea229721b19d28e1db36
Saidul Mursalin <itzmonir@gmail.com>
2023-08-07 12:50:32 +0600

    BadData(Negative number) finder

---
+++ module-22/bad-data.js
@@ -0,0 +1,30 @@
+/*
+#   Negative number counter
+*/
+
+
+// Function to find bad data
+function findingBadData(dataArr) {
+    let badDataCounter = 0;
+    for (element of dataArr) {
+        if (element < 0) {
+            badDataCounter++;
+        }
+    }
+    return badDataCounter;
+}
+
+// Input data array
+const inputOne = [1, 2, 5];
+const inputTwo = [2, -5, -7, -13];
+const inputThree = [-4, -9, -5, -33, -55];
+
+// Calling the function
+const badDataOne = findingBadData(inputOne);
+const badDataTwo = findingBadData(inputTwo);
+const badDataThree = findingBadData(inputThree);
+
+// Printing the result
+console.log("Output of the input 1:", badDataOne);
+console.log("Output of the input 2:", badDataTwo);
+console.log("Output of the input 3:", badDataThree);
