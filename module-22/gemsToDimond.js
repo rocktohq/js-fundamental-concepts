9fccae34cc514aefc4f034c5dc2788fa0957747e
Saidul Mursalin <itzmonir@gmail.com>
2023-08-08 01:23:20 +0600

    Validation Updated

--- module-22/gemsToDimond.js
+++ module-22/gemsToDimond.js
@@ -9,11 +9,15 @@
 
 // Main function to calculate
 function gemsToDimond(gems1, gems2, gems3) {
+    // Validation section
     if (typeof gems1 !== "number" || typeof gems2 !== "number" || typeof gems3 !== "number") {
         return "Error: Please provide valid number";
     } else if (gems1 < 0 || gems2 < 0 || gems3 < 0) {
         return "Error: Please provide positive number";
+    } else if (gems1 % 1 !== 0 || gems2 % 1 !== 0 || gems3 % 1 !== 0) {
+        return "Error: Please provide integer number";
     } else {
+        // If input numbers are valid
         const totalDimonds = (gems1 * 21) + (gems2 * 32) + (gems3 * 43);
         if (totalDimonds > 2000) {
             return totalDimonds - 2000;
